0b3df5f2397d0aaeb6d4f52803bfd8507ff09409
madaghafuri <mada.ghafuri@gmail.com>
2024-06-06 10:05:03 +0700

    feat: add seekbar and progress bar

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { fetchFile, toBlobURL } from "@ffmpeg/util";
 
 let ffmpeg: FFmpeg | null = null;
 let tempFile: File;
+let media_duration: number;
 
 const load = async () => {
   const baseUrl = "https://unpkg.com/@ffmpeg/core@0.12.6/dist/esm";
@@ -11,6 +12,28 @@ const load = async () => {
     ffmpeg = new FFmpeg();
     ffmpeg.on("log", ({ message }) => {
       console.log(message);
+      if (message.includes("Duration")) {
+        const [duration] = message.split(",");
+        const [_, time] = duration.trim().split(" ");
+        let [hours, minutes, seconds] = time.split(":");
+        const parsedHours = parseInt(hours) * 60 * 60;
+        const parsedMinutes = parseInt(minutes) * 60;
+        const parsedSeconds = parseInt(seconds);
+        const totalDuration = parsedHours + parsedMinutes + parsedSeconds;
+        media_duration = totalDuration;
+
+        const seekStart = document.getElementById(
+          "seek-start"
+        ) as HTMLInputElement;
+        const seekEnd = document.getElementById("seek-end") as HTMLInputElement;
+
+        seekStart.max = totalDuration.toString();
+        seekStart.disabled = false;
+        seekStart.value = "0";
+        seekEnd.max = totalDuration.toString();
+        seekEnd.disabled = false;
+        seekEnd.value = "5";
+      }
     });
     ffmpeg.on("progress", ({ progress }) => {
       document.getElementById("progress")!.innerHTML = (progress * 100).toFixed(
@@ -36,14 +59,17 @@ const load = async () => {
   }
 };
 
-const trim = async ({ target: { files } }: { target: { files: FileList } }) => {
+const changeFile = async ({
+  target: { files },
+}: {
+  target: { files: FileList };
+}) => {
   if (!ffmpeg) return;
   const { name } = files[0];
   tempFile = files[0];
 
-  console.log(tempFile);
-
   await ffmpeg.writeFile(name, await fetchFile(files[0]));
+  await ffmpeg.exec(["-i", name]);
   // await ffmpeg.exec([
   //   "-i",
   //   name,
@@ -61,20 +87,31 @@ const trim = async ({ target: { files } }: { target: { files: FileList } }) => {
   const video = document.getElementById("vid") as HTMLVideoElement;
   const url = URL.createObjectURL(files[0]);
   video.src = url;
+
+  const progressBar = document.getElementById(
+    "progress-bar"
+  ) as HTMLProgressElement;
+  progressBar.value = 0;
+  progressBar.innerHTML = "0";
 };
 
 const generateGif = async () => {
   if (!ffmpeg || !tempFile) return;
 
+  const seekStart = document.getElementById("seek-start") as HTMLInputElement;
+  const seekEnd = document.getElementById("seek-end") as HTMLInputElement;
+
   await ffmpeg.exec([
     "-i",
     tempFile.name,
     "-ss",
-    "0",
+    seekStart.value.toString(),
     "-to",
-    "5",
+    seekEnd.value.toString(),
     "-s",
     "320:240",
+    "-r",
+    "24",
     "output.gif",
   ]);
 
@@ -93,13 +130,17 @@ const generateGif = async () => {
 };
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <div class="flex flex-col gap-2">
+  <div class="w-[60vw]">
+    <h1>Vid2GIF</h1>
+    <div class="flex flex-col justify-center items-center gap-2">
       <button id="loader" type="button">Load FFMPEG</button>
       <video controls id="vid" class="rounded-md"></video>
       <input id="file-input" disabled type="file" accept="video/*" class="p-2" />
-      <label>Time</label>
-      <progress id="progress-bar" max="100" min="0" class="w-full"></progress>
+      <label>Start</label>
+      <input type="range" class="w-full" id="seek-start" disabled />
+      <label>End</label>
+      <input type="range" class="w-full" id="seek-end" disabled />
+      <progress id="progress-bar" max="100" min="0" class="w-full rounded"></progress>
     </div>
     <p id="progress"></p>
     <button id="gen-gif">Generate GIF</button
@@ -108,5 +149,5 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 
 document.getElementById("loader")?.addEventListener("click", load);
 //@ts-ignore
-document.getElementById("file-input")?.addEventListener("change", trim);
+document.getElementById("file-input")?.addEventListener("change", changeFile);
 document.getElementById("gen-gif")?.addEventListener("click", generateGif);
